f15c85f48f78d34fea62aa6eb7f4f1d3d0877dc4
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2021-12-21 00:41:15 +0100

    Initialization of project, added API connection and featured heroes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Nav from './Components/Nav/Nav';
+import { getBasicHeroInfoById } from './requests';
+
+const featuredHeroesIds = [620, 502, 10];
 
 function App() {
+  useEffect(() => {
+    fetchAndRenderFeaturedHeroes();
+  }, []);
+
+  const [featuredHeroesList, setFeaturedHeroesList] = useState([]);
+
+  const fetchAndRenderFeaturedHeroes = async () => {
+    const heroes = [];
+    for (const heroId of featuredHeroesIds) {
+      const hero = await getBasicHeroInfoById(heroId);
+      heroes.push(hero);
+    }
+    setFeaturedHeroesList(heroes);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <main>
+        <section className='featured__hero'>
+          {featuredHeroesList.map(({ name, imgUrl, powerstats }) => (
+            <div>
+              <h2>{name}</h2>
+              <img src={imgUrl} alt={name} />
+              <div className='featured__hero__stats'>
+                <div>
+                  <p>{powerstats.combat}</p>
+                </div>
+                <div>
+                  <p>{powerstats.durability}</p>
+                </div>
+                <div>
+                  <p>{powerstats.intelligence}</p>
+                </div>
+                <div>
+                  <p>{powerstats.speed}</p>
+                </div>
+                <div>
+                  <p>{powerstats.strength}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
---
+++ src/requests.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const ACCESS_TOKEN = 4911377808896495;
+const API_URL = `https://superheroapi.com/api/${ACCESS_TOKEN}`;
+
+export const getBasicHeroInfoById = async id => {
+    const { data: powerstats } = await axios.get(`${API_URL}/${id}/powerstats`);
+    const { data: image } = await axios.get(`${API_URL}/${id}/image`);
+    
+    return { name: powerstats.name, powerstats, imgUrl: image.url };
+}

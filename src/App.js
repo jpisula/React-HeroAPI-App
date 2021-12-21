0f9365cded34b9db30bad4234f786a485182e293
Jakub Pisula <jakubpisula@macbook-pro-jakub-2.home>
2021-12-21 01:32:35 +0100

    Added routing to the app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import React from 'react';
 import './App.css';
 import FeaturedHeroes from './Components/FeaturedHeroes/FeaturedHeroes';
 import Nav from './Components/Nav/Nav';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
       <Nav />
       <main>
         <div className='container'>
-          <FeaturedHeroes />
+          <Routes>
+            <Route exact path='/' element={<FeaturedHeroes />} />
+            <Route path='/search/:name' element={<FeaturedHeroes />} />
+            <Route path='/hero/:id' element={<FeaturedHeroes />} />
+            <Route path="*" element={<FeaturedHeroes/>}/>
+          </Routes>
         </div>
       </main>
-      <footer></footer>
+      <footer>
+        <div className="container">
+          <p>This content is kindly provided by <a href="https://superheroapi.com/">Superhero API</a></p>
+        </div>
+      </footer>
+    </Router>
+      
     </>
   );
 }
